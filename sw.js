1d693989404124d6880612ae0b4ed43d975dc7e9
chupper100 <phamminhdat223@gmail.com>
2022-05-01 10:59:51 +0700

    sw: cache

--- sw.js
+++ sw.js
@@ -1,5 +1,22 @@
+const staticCacheName = 'site-static';
+const assets = [
+    '/',
+    '/index.html',
+    '/assets/js/app.js',
+    '/assets/script.js',
+    '/assets/style.css',
+    '/assets/icons/icon-512.png',
+    'https://fonts.googleapis.com',
+    'https://fonts.gstatic.com',
+    'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap',
+];
+
 self.addEventListener('install', (e) => {
     console.log('sw has been installed', e);
+    caches.open(staticCacheName).then((cache) => {
+        console.log('Caching shell assets');
+        cache.addAll(assets);
+    });
 });
 
 self.addEventListener('activate', (e) => {
