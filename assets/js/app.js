859a912bdd70d7cb2c1470c30eccc591d41e0380
chupper100 <phamminhdat223@gmail.com>
2022-05-01 09:27:15 +0700

    sw

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,34 @@
-const registerServiceWorker = async () => {
-    if ('serviceWorker' in navigator) {
-        try {
-            const registration = await navigator.serviceWorker.register(
-                '/sw-test/sw.js',
-                {
-                    scope: '/sw-test/',
-                }
-            );
-            if (registration.installing) {
-                console.log('Service worker installing');
-            } else if (registration.waiting) {
-                console.log('Service worker installed');
-            } else if (registration.active) {
-                console.log('Service worker active');
-            }
-        } catch (error) {
-            console.error(`Registration failed with ${error}`);
-        }
-    }
-};
+// const registerServiceWorker = async () => {
+//     if ('serviceWorker' in navigator) {
+//         try {
+//             const registration = await navigator.serviceWorker.register(
+//                 '/sw-test/sw.js',
+//                 {
+//                     scope: '/sw-test/',
+//                 }
+//             );
+//             if (registration.installing) {
+//                 console.log('Service worker installing');
+//             } else if (registration.waiting) {
+//                 console.log('Service worker installed');
+//             } else if (registration.active) {
+//                 console.log('Service worker active');
+//             }
+//         } catch (error) {
+//             console.error(`Registration failed with ${error}`);
+//         }
+//     }
+// };
 
-// ...
+// // ...
 
-registerServiceWorker();
+// registerServiceWorker();
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/sw.js')
+        .then((reg) =>
+            console
+                .log('sw registered', reg)
+                .catch((err) => console.log('sw not registered', err))
+        );
+}
