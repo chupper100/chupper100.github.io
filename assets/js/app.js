32a15a0124573d97c629a1a3a60e270ed2c04c12
chupper100 <phamminhdat223@gmail.com>
2022-05-01 09:33:22 +0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,15 +20,10 @@
 //     }
 // };
 
-// // ...
-
 // registerServiceWorker();
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/sw.js')
-        .then((reg) =>
-            console
-                .log('sw registered', reg)
-                .catch((err) => console.log('sw not registered', err))
-        );
+        .then((reg) => console.log('sw registered', reg))
+        .catch((err) => console.log('sw not registered', err));
 }
